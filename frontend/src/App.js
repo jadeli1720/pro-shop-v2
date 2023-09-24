35e6da4bc016ea556205228f64b886178dc3f53a
jadeli1720 <jadeli1720@gmail.com>
2023-09-23 23:37:36 -0600

    Added product data, images, and component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container> 
-          <h1>Welcome to ProShop</h1>
+          <Homepage />
         </Container>
       </main>
       <Footer />
