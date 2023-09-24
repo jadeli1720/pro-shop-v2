b4ff5584883e2cd82572e1a0d6ee0fb7f0439a5a
jadeli1720 <jadeli1720@gmail.com>
2023-09-24 00:07:02 -0600

    Implemented the router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements,  
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Homepage from './pages/Homepage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      {/* The index={true} prevents multiple pages don't render at the same time */}
+      <Route index={true} path='/' element={<Homepage />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 
-reportWebVitals();
