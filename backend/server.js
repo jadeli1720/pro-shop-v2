659cbf43769ba3ab3d4400e0daaf6f5da941e1d8
jadeli1720 <jadeli1720@gmail.com>
2023-09-24 14:08:23 -0600

    Using mongoose to connect with the proshop mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +24,6 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
-
 
 
 app.listen(port, () => {  
