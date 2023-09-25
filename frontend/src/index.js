7423edcfa7072f2114faf53cd0edd24b93a92c99
jadeli1720 <jadeli1720@gmail.com>
2023-09-24 21:23:06 -0600

    Added redux and redux toolkit. Set up the store, added the provider to the index.js file, and added the url constants

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter, 
-  createRoutesFromElements,  
-  Route,
-  RouterProvider,
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
-
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
-			{/* The index={true} prevents multiple pages don't render at the same time */}
+			{/* The index={true} prevents multiple pages rendering at the same time */}
 			<Route index={true} path='/' element={<HomePage />} />
-			<Route  path='/product/:id' element={<ProductPage />} />
+			<Route path='/product/:id' element={<ProductPage />} />
 		</Route>
 	)
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
 );
-
-
