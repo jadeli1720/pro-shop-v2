56dc31fa4690f6ad01a3b6997f1db5b76d023a0b
jadeli1720 <jadeli1720@gmail.com>
2023-09-25 21:07:06 -0600

    added jsonwebtoken to create a secure token/cookie that is set on the server

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,40 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 //@desc Auth user & get token
 //@route POST /api/users/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await User.findOne({email});
+	const user = await User.findOne({ email });
 
-  if(user && (await user.matchPassword(password))){
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
+	if (user && (await user.matchPassword(password))) {
+		//.sign creates a token with the user id, the secret key, and an expiration time-frame (30 days)
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+			expiresIn: '30d',
+		});
+
+		//Set JWT as an HTTP-Only cookie
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV !== 'development',
+			sameSite: 'strict',
+			//below is in milliseconds but needs to be converted to days
+			maxAge: 30 * 24 * 60 * 60 * 1000, //30 Days
+		});
+
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid email or password');
+	}
 });
 
 //@desc Register User
