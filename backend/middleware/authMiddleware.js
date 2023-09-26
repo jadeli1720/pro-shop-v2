27dc6b5916ae2571bef0cdbaa72b1fddc3c8a166
jadeli1720 <jadeli1720@gmail.com>
2023-09-25 21:36:17 -0600

    added protected and admin check to routes in server.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,30 +1,30 @@
 import jwt from 'jsonwebtoken';
-import asyncHandler from './asyncHandler';
-import User from '../models/userModel';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
 
 
 //Protect routes
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+	let token;
 
-  //Read the JWT from the cookie
-  token= req.cookies.jwt;
+	//Read the JWT from the cookie
+	token = req.cookies.jwt;
 
-  if(token) {
-    try {
-      //decode the token to get the user id
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select('-password');
-      next()
-    } catch(error){
-      console.log(error);
-      res.status(401);
-		  throw new Error('Not authorized, token failed');
-    }
-  }else {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
+	if (token) {
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+			req.user = await User.findById(decoded.userId).select('-password');
+
+			next();
+		} catch (error) {
+			console.error(error);
+			res.status(401);
+			throw new Error('Not authorized, token failed');
+		}
+	} else {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
 });
 
 //Check if user is an admin
