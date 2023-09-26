c981df23dec0bbeb1abdc9875516614a48db243a
jadeli1720 <jadeli1720@gmail.com>
2023-09-25 23:08:07 -0600

    added the auth and user slices.

---
+++ frontend/src/slices/usersSlice.js
@@ -0,0 +1,21 @@
+import { USERS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+
+//injecting endpoints in the parent apiSlice
+export const userApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		//post request
+		login: builder.mutation({
+			query: (data) => ({
+				url: `${USERS_URL}/login`,
+        method: 'POST',
+        body: data,
+			}),
+		}),
+	}),
+});
+
+export const { 
+  useGetLoginMutation,
+} = userApiSlice;
