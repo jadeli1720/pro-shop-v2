8498025ff308d825f4fdb796b796628a99b3ca57
jadeli1720 <jadeli1720@gmail.com>
2023-09-27 12:19:14 -0600

    Added shipping form to shipping page

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -8,7 +8,60 @@ const ShippingPage = () => {
 	const [postalCode, setPostalCode] = useState('');
 	const [country, setCountry] = useState('');
 
-	return <div>ShippingPage</div>;
+	const submitHandler = (e) => {
+		e.preventDefault();
+		console.log('submit');
+	};
+
+	return (
+		<FormContainer>
+			<h1>Shipping</h1>
+			<Form onSubmit={submitHandler}>
+				{/* address */}
+				<Form.Group controlId='address' className='my-2'>
+					<Form.Label>Address</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter address'
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				{/* city */}
+				<Form.Group controlId='city' className='my-2'>
+					<Form.Label>City</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter city'
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				{/* postal code */}
+				<Form.Group controlId='postalCode' className='my-2'>
+					<Form.Label>Postal Code</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter postal code'
+						value={postalCode}
+						onChange={(e) => setPostalCode(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				{/* country */}
+				<Form.Group controlId='country' className='my-2'>
+					<Form.Label>Country</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter county'
+						value={country}
+						onChange={(e) => setCountry(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+        <Button type='submit' variant='primary' className='my-2' >Continue</Button>
+			</Form>
+
+		</FormContainer>
+	);
 };
 
 export default ShippingPage;
