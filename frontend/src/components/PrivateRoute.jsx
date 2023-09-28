492a2166b5bf5d29b2c17664c5bfcb7250234bf3
jadeli1720 <jadeli1720@gmail.com>
2023-09-28 16:47:45 -0600

    Added checkout steps component

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,9 +2,9 @@ import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
+	const { userInfo } = useSelector((state) => state.auth);
 
-  return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
-}
+	return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -4,28 +4,32 @@ import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingPage = () => {
-  const cart = useSelector((state) => state.cart);
-  const { shippingAddress } = cart;
+	const cart = useSelector((state) => state.cart);
+	const { shippingAddress } = cart;
 
 	const [address, setAddress] = useState(shippingAddress?.address || '');
 	const [city, setCity] = useState(shippingAddress?.city || '');
-	const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+	const [postalCode, setPostalCode] = useState(
+		shippingAddress?.postalCode || ''
+	);
 	const [country, setCountry] = useState(shippingAddress?.country || '');
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-    dispatch(saveShippingAddress({ address, city, postalCode, country }));
-    navigate('/payment');
+		dispatch(saveShippingAddress({ address, city, postalCode, country }));
+		navigate('/payment');
 	};
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
+
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				{/* address */}
@@ -68,9 +72,10 @@ const ShippingPage = () => {
 						onChange={(e) => setCountry(e.target.value)}
 					></Form.Control>
 				</Form.Group>
-        <Button type='submit' variant='primary' className='my-2' >Continue</Button>
+				<Button type='submit' variant='primary' className='my-2'>
+					Continue
+				</Button>
 			</Form>
-
 		</FormContainer>
 	);
 };
