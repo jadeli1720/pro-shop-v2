6728328481943407177a2b8a2a26f5c42401d599
jadeli1720 <jadeli1720@gmail.com>
2023-09-28 16:30:26 -0600

    Added the shipping screen and functionality as far as saving the address data to local storage.

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
+import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingPage = () => {
-	const [address, setAddress] = useState('');
-	const [city, setCity] = useState('');
-	const [postalCode, setPostalCode] = useState('');
-	const [country, setCountry] = useState('');
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+	const [address, setAddress] = useState(shippingAddress?.address || '');
+	const [city, setCity] = useState(shippingAddress?.city || '');
+	const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+	const [country, setCountry] = useState(shippingAddress?.country || '');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		console.log('submit');
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate('/payment');
 	};
 
 	return (
